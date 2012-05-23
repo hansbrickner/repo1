05077425a6f94eba09fce3e20148a78ed56807ef
Hans Brickner <hans.brickner@ericsson.com>
2012-05-23 13:35:12 +0200

    Change

--- src/main.cpp
+++ src/main.cpp
@@ -7,5 +7,7 @@ int main(){
 	std::cout << "Hello, World!" << std::endl;
 	
 	std::cout << "result " << add(4,2) << std::endl;
+	
+	std::cout << "Result " << sub(9,6) << std::endl;
 	return 0;	
 }
--- src/main.h
+++ src/main.h
@@ -5,4 +5,8 @@ int add(int a,int b){
 	return a+b;	
 }
 
+int sub(int a, int b){
+	return a-b;	
+}
+
 #endif
